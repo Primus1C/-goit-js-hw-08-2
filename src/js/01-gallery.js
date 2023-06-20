6a72ff87871d6c8cd4acd2e4a38225c8d4bb300b
Artur <110535829+Primus1C@users.noreply.github.com>
2023-06-20 13:05:33 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Change code below this linenp
 
 console.log(galleryItems);
+
+
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(element => {
+  const li = document.createElement('li');
+  li.innerHTML = `<a class="gallery__link">
+                        <img
+                            class="gallery__image"
+                            src="${element.preview}"
+                            data-source="${element.original}"
+                            alt="${element.description}"
+                        />
+                    </a>`;
+  li.className = 'gallery__item';
+  gallery.append(li);
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+})
