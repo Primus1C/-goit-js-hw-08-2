cd72f043b3b84e737ca205100bbe3da568155e07
Primus1C <110535829+Primus1C@users.noreply.github.com>
2023-06-22 10:53:50 +0000

    Deploying to gh-pages from @ Primus1C/-goit-js-hw-08-2@7c9199a4f89b43c3af6745b472171ac0eedd101b 🚀

--- 03-feedback.5549cad2.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form"),document.querySelector(".message"),document.querySelector(".email");
-//# sourceMappingURL=03-feedback.5549cad2.js.map
--- 03-feedback.d2ae8693.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form"),document.querySelector(".message"),document.querySelector(".email");
-//# sourceMappingURL=03-feedback.d2ae8693.js.map
