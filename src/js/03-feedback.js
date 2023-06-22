7c9199a4f89b43c3af6745b472171ac0eedd101b
Artur <110535829+Primus1C@users.noreply.github.com>
2023-06-22 12:53:26 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,37 @@
+import throttle from 'lodash.throttle';
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('.message'),
   emailarea: document.querySelector('.email'),
 };
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 1000));
+
+const STORAGE_KEY = 'feedback-form-state';
+
+populateAreas();
+
 function onFormSubmit(evt) {
   evt.preventDefault();
-  console.log('submit...');
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInput(evt) {
+  feedback = JSON.stringify({
+    message: refs.textarea.value,
+    email: refs.emailarea.value,
+  });
+  localStorage.setItem(STORAGE_KEY, feedback);
+}
+
+function populateAreas() {
+  const savedMsg = localStorage.getItem(STORAGE_KEY);
+  if (savedMsg) {
+    msg = JSON.parse(savedMsg);
+    refs.emailarea.value = msg.email;
+    refs.textarea.value = msg.message;
+  }
 }
