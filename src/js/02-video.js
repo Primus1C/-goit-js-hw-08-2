a1483822080d153eeccdb74c7a8c57d9c13f9643
Artur <110535829+Primus1C@users.noreply.github.com>
2023-06-23 11:01:48 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,22 @@
 import throttle from "lodash.throttle";
 import Player from '@vimeo/player';
 
-const SAVED_CURRENT_TIME_KEY = 'videoplayer-current-time';
+const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(videoPlayerTimeUpdate, 1000));
 
+populatePlayer();
+
 function videoPlayerTimeUpdate(data) {
-
-
+  window.localStorage.setItem(STORAGE_KEY, data.seconds);
+  console.log(data.seconds);
 }
 
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    window.localStorage.setItem('videoplayer-current-time', data.seconds);
-  }, 250)
-);
-
+function populatePlayer() {
+const savedPos = localStorage.getItem(STORAGE_KEY);
+  if (savedPos) {
+    player.setCurrentTime(savedPos);
+  }
+}
